515976e19336e81ea0765566c2bec19904864a23
joshuathomas3 <joshuathomas60@outlook.com>
2025-05-16 22:25:39 +0100

    Changed domain list and table layout

--- dnstest.sh
+++ dnstest.sh
@@ -9,33 +9,26 @@ NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/
 
 PROVIDERSV4="
 1.1.1.1#cloudflare 
-4.2.2.1#level3 
 8.8.8.8#google 
 9.9.9.9#quad9 
-80.80.80.80#freenom 
-208.67.222.123#opendns 
-199.85.126.20#norton 
-185.228.168.168#cleanbrowsing 
-77.88.8.7#yandex 
-176.103.130.132#adguard 
-156.154.70.3#neustar 
-8.26.56.26#comodo
-45.90.28.202#nextdns
+94.140.14.14#adguard
+208.67.222.222#opendns
+185.228.168.9#cleanbrowsing
+76.76.2.0#controld
 "
 
 PROVIDERSV6="
 2606:4700:4700::1111#cloudflare-v6
 2001:4860:4860::8888#google-v6
-2620:fe::fe#quad9-v6
-2620:119:35::35#opendns-v6
-2a0d:2a00:1::1#cleanbrowsing-v6
-2a02:6b8::feed:0ff#yandex-v6
+2620:fe::fe#quad-9
 2a00:5a60::ad1:0ff#adguard-v6
-2610:a1:1018::3#neustar-v6
+2620:0:ccc::2#opendns-v6
+2a0d:2a00:1::2#cleanbrowsing-v6
+2606:1a40::#controld-v6
 "
 
 # Testing for IPv6
-$dig +short +tries=1 +time=2 +stats @2a0d:2a00:1::1 www.google.com |grep 216.239.38.120 >/dev/null 2>&1
+$dig +short +tries=1 +time=2 +stats @2607:f8b0:4003:c00::6a www.google.com |grep 216.239.38.120 >/dev/null 2>&1
 if [ $? = 0 ]; then
     hasipv6="true"
 fi
@@ -64,18 +57,32 @@ fi
     
 
 # Domains to test. Duplicated domains are ok
-DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
-
+DOMAINS2TEST="www.google.com www.youtube.com www.facebook.com www.instagram.com chatgpt.com x.com www.whatsapp.com wikipedia.org reddit.com yahoo.co.jp"
+# Most visted sites in the world as of may 2025 according to Similarweb and Semrush
 
 totaldomains=0
-printf "%-21s" ""
+header=""
+separator=""
+
+# Add table header for DNS resolver
+header=$(printf "%-21s" "DNS Resolver")
+separator=$(printf "%-21s" "" | tr ' ' '-')
 for d in $DOMAINS2TEST; do
     totaldomains=$((totaldomains + 1))
-    printf "%-8s" "test$totaldomains"
+    header="$header$(printf "%-8s" "test$totaldomains")"
+    separator="$separator$(printf "%-8s" "" | tr ' ' '-')"
 done
-printf "%-8s" "Average"
-echo ""
+header="$header$(printf "%-8s" "Average")"
+separator="$separator$(printf "%-8s" "" | tr ' ' '-')"
 
+echo "$header"
+echo "$separator"
+
+# ANSI color codes
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 for p in $NAMESERVERS $providerstotest; do
     pip=${p%%#*}
@@ -86,19 +93,31 @@ for p in $NAMESERVERS $providerstotest; do
     for d in $DOMAINS2TEST; do
         ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
         if [ -z "$ttime" ]; then
-	        #let's have time out be 1s = 1000ms
-	        ttime=1000
+            #let's have time out be 1s = 1000ms
+            ttime=1000
         elif [ "x$ttime" = "x0" ]; then
-	        ttime=1
-	    fi
+            ttime=1
+        fi
 
         printf "%-8s" "$ttime ms"
         ftime=$((ftime + ttime))
     done
     avg=`bc -l <<< "scale=2; $ftime/$totaldomains"`
+    avg_int=${avg%.*}
 
-    echo "  $avg"
+    # Colorize average
+    if (( avg_int < 20 )); then
+        color=$GREEN
+    elif (( avg_int < 150 )); then
+        color=$YELLOW
+    else
+        color=$RED
+    fi
+
+    printf "  ${color}%-8s${NC}\n" "$avg"
 done
+
+echo "$separator"
 
 
 exit 0;
